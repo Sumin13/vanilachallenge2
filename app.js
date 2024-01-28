882808b834f03dc562c4b83560d8c4436a35dc1e
Sumin13 <su184913@icloud.com>
2024-01-28 18:33:04 +0000

    28.01.24

---
+++ app.js
@@ -0,0 +1,32 @@
+const firstNumber = document.getElementById("computer-number");
+const firstNumberChose = firstNumber.querySelector("input");
+const youChoose = document.getElementById("human-number");
+const finalNumber = youChoose.querySelector("input");
+const resultValue = document.querySelector("h5");
+
+let numberValue;
+
+function chosenNumber(event) {
+  event.preventDefault();
+  numberValue = firstNumberChose.value;
+  localStorage.setItem("number", numberValue);
+}
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * parseInt(firstNumberChose.value, 10));
+}
+
+const computerGen = generateRandomNumber();
+
+function gameResult(event) {
+  event.preventDefault();
+  const secondNumber = finalNumber.value;
+
+  if (parseInt(secondNumber, 10) === parseInt(numberValue, 10)) {
+    resultValue.innerText = "You won!";
+  } else {
+    resultValue.innerText = `Wrong! You chose ${secondNumber} and computer chose ${numberValue}`;
+  }
+}
+firstNumber.addEventListener("submit", chosenNumber);
+youChoose.addEventListener("submit", gameResult);
